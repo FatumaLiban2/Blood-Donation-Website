69db13b5a026e50def2954615fa5b725fc42fd03
Yukih <158501974+Y-u-k-i-h@users.noreply.github.com>
2025-10-22 12:53:08 +0300

    Modified mail.php

--- config/config.php
+++ config/config.php
@@ -14,6 +14,13 @@ define("DB_SSLMODE", $_ENV['PGSSLMODE']);
 define("DB_CHANNELBINDING", $_ENV['PGCHANNELBINDING']);
 define("DB_PORT", $_ENV['PGPORT']);
 
+// PHP Mailer SMTP configuration
+define("SMTP_HOST", $_ENV['SMTP_HOST']);
+define("SMTP_USER", $_ENV['SMTP_USER']);
+define("SMTP_PASSWORD", $_ENV['SMTP_PASSWORD']);
+define("SMTP_PORT", $_ENV['SMTP_PORT']);
+define("SMTP_ENCRYPTION", $_ENV['SMTP_ENCRYPTION']);
+
 // Cryptographic keys
 define("JWT_PRIVATE_KEY_ENCODED", $_ENV['JWT_PRIVATE_KEY_ENCODED']);
 define("JWT_PUBLIC_KEY_ENCODED", $_ENV['JWT_PUBLIC_KEY_ENCODED']);
